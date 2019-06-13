8ee9dc304af67a47b2a64bafdc460ef78e1d03f5
ctrlaltdelete44 <daviesanthonye1234@gmail.com>
2019-06-13 17:08:03 +0100

    cleaned flash showing. force ssl enabled

--- Dungeons/test/integration/accounts_signup_test.rb
+++ Dungeons/test/integration/accounts_signup_test.rb
@@ -11,8 +11,20 @@ class AccountsSignupTest < ActionDispatch::IntegrationTest
             end
             assert_template 'accounts/new'
 
-            assert_select "p.error-msg", "Email is invalid"
-            assert_select "p.error-msg", "Password confirmation doesn't match Password"
-            assert_select "p.error-msg", "Password is too short (minimum is 8 characters)"
+            assert_select "p.danger-msg", "Email is invalid"
+            assert_select "p.danger-msg", "Password confirmation doesn't match Password"
+            assert_select "p.danger-msg", "Password is too short (minimum is 8 characters)"
+    end
+
+    test "valid signup information" do
+        get signup_path
+        assert_difference 'Account.count', 1 do
+            post signup_path, params: { account: { display_name: "My display name",
+                                                                                                 email: "user@example.com",
+                                                                                                 password: "password",
+                                                                                                 password_confirmation: "password" } }
+        end
+        follow_redirect!
+        assert_template 'accounts/show'
     end
 end
