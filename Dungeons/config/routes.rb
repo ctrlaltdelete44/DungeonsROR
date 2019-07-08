998fddfed33750db45abd9c0cacf83dce17824f5
Anthony <daviesanthonye1234@gmail.com>
2019-07-08 15:40:33 +0100

    fixing merge conflicts

--- Dungeons/config/routes.rb
+++ Dungeons/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'password_resets/new'
-  get 'password_resets/edit'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
@@ -11,6 +9,5 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   resources :accounts
   resources :account_activations, only: [:edit]
-  resources :password_resets, only: [:new, :create, :edit, :update]
   root 'static_pages#home'
 end
