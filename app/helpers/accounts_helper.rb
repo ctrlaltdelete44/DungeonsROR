34ef7fd6fe8c3d92050610bde1bf1e57e5431049
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2019-07-24 10:42:02 +0100

    changed gravatar_for helper so that size refers to size of image tag. removed old stylesheets, now can clearly see what to fix

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -2,10 +2,10 @@
 
 module AccountsHelper
   # returns gravatar for given email
-  def gravatar_for(email, options = { size: 80 })
+  def gravatar_for(email, options = { size: 4 })
     gravatar_id = Digest::MD5.hexdigest(email.downcase)
     size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: 'Profile picture', class: 'gravatar')
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=300"
+    image_tag(gravatar_url, alt: 'Profile picture', class: "br-100 w#{size} h#{size}")
   end
 end
