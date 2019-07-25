594e35e32999eb21a168c8bccc75f28b72d3784f
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2019-07-25 10:44:43 +0100

    migration to remove old columns added

---
+++ db/migrate/20190725094226_remove_manual_validation_from_accounts.rb
@@ -0,0 +1,12 @@
+class RemoveManualValidationFromAccounts < ActiveRecord::Migration[5.2]
+  def change
+    #remove_column :microposts, :picture, :string
+    remove_column :accounts, :password_digest, :string
+    remove_column :accounts, :remember_digest, :string
+    remove_column :accounts, :activation_digest, :string
+    remove_column :accounts, :activated, :boolean, default: false
+    remove_column :accounts, :activated_at, :datetime
+    remove_column :accounts, :reset_digest, :string
+    remove_column :accounts, :reset_sent_at, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_122412) do
+ActiveRecord::Schema.define(version: 2019_07_25_094226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,14 +20,7 @@ ActiveRecord::Schema.define(version: 2019_07_22_122412) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.string "remember_digest"
     t.boolean "admin", default: false
-    t.string "activation_digest"
-    t.boolean "activated", default: false
-    t.datetime "activated_at"
-    t.string "reset_digest"
-    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_accounts_on_email"
   end
 
